4de89975c665c4a1c81890fe63ab025f071cf55f
abidh5080 <abidh5080@gmail.com>
2022-03-02 15:52:03 +0800

    change something

--- bank.js
+++ bank.js
@@ -5,46 +5,40 @@
     const nextPage = document.getElementById('nextPage')
         nextPage.style.display = 'block'
 });
-
-    const addDepositBtn = document.getElementById('addDepositBtn');
-
-    addDepositBtn.addEventListener('click', function () {
-    const currentDeposit = document.getElementById('depositAmount').value;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    if (currentDepositNumber <= 0) {
-        return null;
-    }
-
-    let currentDepositAmount = document.getElementById('currentDeposit').innerText;
-    let depositAmount = parseFloat(currentDepositAmount);
-    let depositAdBalance = currentDepositNumber + depositAmount;
+// function(1) for take input value
+function getInputNumber(id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+// function(2) for take inner text value
+function getInnerText(id) {
+    let currentBalance = document.getElementById(id).innerText;
+let balanceNumber = parseFloat(currentBalance);
+return balanceNumber;
+}
+document.getElementById('addDepositBtn').addEventListener('click', function () {
+    const deposit = getInputNumber('depositAmount');
+    const depositAmount = getInnerText('currentDeposit');
+    let depositAdBalance = deposit + depositAmount;
 
     document.getElementById('currentDeposit').innerText = depositAdBalance;
     document.getElementById('depositAmount').value = "";
 
-    let currentBalance = document.getElementById('currentBalance').innerText;
-    let balanceNumber = parseFloat(currentBalance);
-    let totalBalance = balanceNumber + currentDepositNumber;
-
+    const balance = getInnerText('currentBalance');
+    let totalBalance = balance + deposit;
     document.getElementById('currentBalance').innerText = totalBalance;
 })
-    document.getElementById('withdrawBtn').addEventListener('click', function(){
-        const withdrawAmount = document.getElementById('withdrawAdd').value;
-        const withdrawAmountNumber = parseFloat(withdrawAmount)
-        if (withdrawAmountNumber <= 0) {
-            return null;
-        }
-        const WithdrawMony = document.getElementById('withdrawMony').innerText;
-        const withdrawNumber = parseFloat(WithdrawMony);
+document.getElementById('withdrawBtn').addEventListener('click', function(){
+    const withdraw = getInputNumber('withdrawAdd');
+    const withdrawNumber = getInnerText('withdrawMony');
+    const balanceNumber = getInnerText('currentBalance');
+    const totalWithdraw = withdrawNumber + withdraw;
 
-        let currentBalance = document.getElementById('currentBalance').innerText;
-        let balanceNumber = parseFloat(currentBalance);
-        
-        const totalWithdraw = withdrawNumber + withdrawAmountNumber;
+    document.getElementById('withdrawMony').innerText =totalWithdraw ;
+    document.getElementById('currentBalance').innerText = balanceNumber - withdraw;
+    document.getElementById('withdrawAdd').value = "";
+    })
+    
 
-        document.getElementById('withdrawMony').innerText =totalWithdraw ;
-        document.getElementById('currentBalance').innerText = balanceNumber - withdrawAmountNumber;
-        document.getElementById('withdrawAdd').value = "";
-        
-    });
     
